b8562b3a4035982fc44a548e47622ee3bf5b46c7
Profiremu23 <profirdani23@gmail.com>
2024-02-15 19:26:27 +0100

    Added colour code matching and fixed game flow

--- mastermind.rb
+++ mastermind.rb
@@ -20,22 +20,24 @@ end
 # Defining the Mastermind game board
 class GameBoard
   attr_accessor :colours, :turn
-  attr_reader :board_state
+  attr_reader :guesser_code
 
   def initialize
-    @board_state = Array.new(4) { Cell.new }
+    @guesser_code = Array.new(4) { Cell.new }
     @colours = %w[red blue yellow purple green orange black white]
     @entered_colour = ' '
+    @ind = 0
     @index = 0
-    @turn = 0
+    @matches = 0
+    @turn = 12
   end
 
   def update_cell(index, colour)
-    board_state[index].symbol = colour
+    guesser_code[index].symbol = colour
   end
 
   def print_board
-    print "\n| #{board_state[0]} | #{board_state[1]} | #{board_state[2]} | #{board_state[3]} |\n"
+    print "\n| #{guesser_code[0]} | #{guesser_code[1]} | #{guesser_code[2]} | #{guesser_code[3]} |\n\n"
   end
 end
 
@@ -46,6 +48,13 @@ class Game < GameBoard
   def initialize
     super
     @colour_code = Array.new(4) { Cell.new }
+  end
+
+  def guesser_code_clearer
+    guesser_code[0].symbol = nil
+    guesser_code[1].symbol = nil
+    guesser_code[2].symbol = nil
+    guesser_code[3].symbol = nil
   end
 
   def colour_code_generator
@@ -59,41 +68,67 @@ class Game < GameBoard
     print "| #{colour_code[0]} | #{colour_code[1]} | #{colour_code[2]} | #{colour_code[3]} |\n\n"
   end
 
+  def colour_code_matching
+    4.times do
+      if @guesser_code[@ind].symbol == colour_code[@ind].symbol
+        @matches += 1
+        @ind += 1
+      else
+        @ind += 1
+      end
+    end
+    puts "#{@matches} colours with their exact locations have been found"
+    @matches = 0
+    @ind = 0
+  end
+
   def mastermind_winning_condition
-    if @board_state == @colour_code
+    if @matches == 4
       puts "Congratulations! You have successfully guessed your given colour code in #{@turn} turns!"
-    elsif turn > 12
+      print_colour_code
+    elsif @matches < 4 && @turn <= 12
+      puts 'Your summitted code does not fully match with the mystery code!'
+      colour_code_matching
+      @turn += 1
+      guesser_code_clearer
+      colour_code_guesser
+    else
       puts 'Sorry! You have failed to successfully guess your given colour code within 12 turns, the solution was:'
       print_colour_code
-    else
-      colour_code_guesser
-      @turn += 1
     end
   end
 
   def colour_code_guesser
-    until @board_state[0].free? == false && @board_state[1].free? == false && @board_state[2].free? == false && @board_state[3].free? == false
-      @index = gets.chomp.to_i
-      until @index.negative? == false && @index < 4
-        puts 'The entered given index value is out of range!'
+    if @turn > 12
+      mastermind_winning_condition
+    else
+      puts "\nGuess #{@turn} out of 12"
+
+      until @guesser_code[0].free? == false && @guesser_code[1].free? == false && @guesser_code[2].free? == false && @guesser_code[3].free? == false
+        print 'Please enter an index to place your colour in: '
         @index = gets.chomp.to_i
-      end
+        until @index.negative? == false && @index < 4
+          puts 'The entered given index value is out of range!'
+          @index = gets.chomp.to_i
+        end
 
-      @entered_colour = gets.chomp
-      until @entered_colour == colours[0] || @entered_colour == colours[1] || @entered_colour == colours[2] || @entered_colour == colours[3] || @entered_colour == colours[4] || @entered_colour == colours[5] || @entered_colour == colours[6] || @entered_colour == colours[7]
-        puts 'The entered given colour is invalid!'
+        print "Please enter a colour for your chosen index (#{@index}): "
         @entered_colour = gets.chomp
-      end
+        until @entered_colour == colours[0] || @entered_colour == colours[1] || @entered_colour == colours[2] || @entered_colour == colours[3] || @entered_colour == colours[4] || @entered_colour == colours[5] || @entered_colour == colours[6] || @entered_colour == colours[7]
+          puts 'The entered given colour is invalid!'
+          @entered_colour = gets.chomp
+        end
 
-      puts update_cell(@index, @entered_colour)
-      print_board
+        puts update_cell(@index, @entered_colour)
+        print_board
+      end
     end
+    mastermind_winning_condition
   end
 
   def mastermind_game
     colour_code_generator
     colour_code_guesser
-    mastermind_winning_condition
   end
 end
 
