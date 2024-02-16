6d5f111c006d8a27403402f625a5ea91749e09df
Profiremu23 <profirdani23@gmail.com>
2024-02-16 20:02:26 +0100

    Did a cut on unnecessary code in preparation for adding a computer player

--- mastermind.rb
+++ mastermind.rb
@@ -1,31 +1,15 @@
 # frozen_string_literal: true
 
-# Defining the cells that makes up the game board
-class Cell
-  attr_accessor :symbol
-
-  def initialize
-    @symbol = nil
-  end
-
-  def free?
-    symbol.nil?
-  end
-
-  def to_s
-    symbol || ' '
-  end
-end
-
 # Defining the Mastermind game board
 class GameBoard
   attr_accessor :colours, :turn
   attr_reader :guesser_code
 
   def initialize
-    @guesser_code = Array.new(4) { Cell.new }
+    @guesser_code = Array.new(4)
     @colours = %w[red blue yellow purple green orange black white]
     @entered_colour = ' '
+    @i = 0
     @ind = 0
     @index = 0
     @matches = 0
@@ -33,7 +17,7 @@ class GameBoard
   end
 
   def update_cell(index, colour)
-    guesser_code[index].symbol = colour
+    guesser_code[index] = colour
   end
 
   def print_board
@@ -47,29 +31,29 @@ class Game < GameBoard
 
   def initialize
     super
-    @colour_code = Array.new(4) { Cell.new }
-    @matching_code = Array.new(4) { Cell.new }
+    @colour_code = Array.new(4)
+    @matching_code = []
   end
 
   def guesser_code_clearer
-    guesser_code[0].symbol = nil
-    guesser_code[1].symbol = nil
-    guesser_code[2].symbol = nil
-    guesser_code[3].symbol = nil
+    guesser_code[0] = nil
+    guesser_code[1] = nil
+    guesser_code[2] = nil
+    guesser_code[3] = nil
   end
 
   def matching_code_clearer
-    matching_code[0].symbol = nil
-    matching_code[1].symbol = nil
-    matching_code[2].symbol = nil
-    matching_code[3].symbol = nil
+    matching_code[0] = nil
+    matching_code[1] = nil
+    matching_code[2] = nil
+    matching_code[3] = nil
   end
 
   def colour_code_generator
-    colour_code[0].symbol = @colours.sample
-    colour_code[1].symbol = @colours.sample
-    colour_code[2].symbol = @colours.sample
-    colour_code[3].symbol = @colours.sample
+    colour_code[0] = @colours.sample
+    colour_code[1] = @colours.sample
+    colour_code[2] = @colours.sample
+    colour_code[3] = @colours.sample
   end
 
   def print_colour_code
@@ -82,17 +66,23 @@ class Game < GameBoard
 
   def colour_code_matching
     4.times do
-      if @guesser_code[@ind].symbol == colour_code[@ind].symbol
-        @matching_code[@ind].symbol = 'white'
+      if @guesser_code[@ind] == colour_code[@ind]
+        @matching_code << 'white'
         @matches += 1
-        @ind += 1
       else
-        @matching_code[@ind].symbol = 'black'
-        @ind += 1
+        4.times do
+          if @colour_code.include?(@guesser_code[@ind]) == true && (@guesser_code[@ind] = colour_code[@i] == false)
+            @matching_code << 'black'
+          else
+            @matching_code << ' '
+          end
+          @i += 1
+        end
       end
+      @ind += 1
     end
     puts "#{@matches} colours with their exact locations have been found"
-    print_matching_code 
+    print_matching_code
     @matches = 0
     @ind = 0
   end
@@ -101,7 +91,7 @@ class Game < GameBoard
     if @matches == 4
       puts "Congratulations! You have successfully guessed your given colour code in #{@turn} turns!"
       print_colour_code
-    elsif @matches < 4 && @turn <= 12
+    elsif @turn < 13 && @matches < 4
       puts 'Your summitted code does not fully match with the mystery code!'
       colour_code_matching
       @turn += 1
@@ -114,29 +104,25 @@ class Game < GameBoard
   end
 
   def colour_code_guesser
-    if @turn > 12
-      mastermind_winning_condition
-    else
-      puts "\nGuess #{@turn} out of 12"
+    puts "\nGuess #{@turn} out of 12"
 
-      until @guesser_code[0].free? == false && @guesser_code[1].free? == false && @guesser_code[2].free? == false && @guesser_code[3].free? == false
-        print 'Please enter an index to place your colour in: '
+    until @guesser_code[0].nil? == false && @guesser_code[1].nil? == false && @guesser_code[2].nil? == false && @guesser_code[3].nil? == false
+      print 'Please enter an index to place your colour in: '
+      @index = gets.chomp.to_i
+      until @index.negative? == false && @index < 4
+        puts 'The entered given index value is out of range!'
         @index = gets.chomp.to_i
-        until @index.negative? == false && @index < 4
-          puts 'The entered given index value is out of range!'
-          @index = gets.chomp.to_i
-        end
-
-        print "Please enter a colour for your chosen index (#{@index}): "
-        @entered_colour = gets.chomp
-        until @entered_colour == colours[0] || @entered_colour == colours[1] || @entered_colour == colours[2] || @entered_colour == colours[3] || @entered_colour == colours[4] || @entered_colour == colours[5] || @entered_colour == colours[6] || @entered_colour == colours[7]
-          puts 'The entered given colour is invalid!'
-          @entered_colour = gets.chomp
-        end
-
-        puts update_cell(@index, @entered_colour)
-        print_board
       end
+
+      print "Please enter a colour for your chosen index (#{@index}): "
+      @entered_colour = gets.chomp
+      until @entered_colour == colours[0] || @entered_colour == colours[1] || @entered_colour == colours[2] || @entered_colour == colours[3] || @entered_colour == colours[4] || @entered_colour == colours[5] || @entered_colour == colours[6] || @entered_colour == colours[7]
+        puts 'The entered given colour is invalid!'
+        @entered_colour = gets.chomp
+      end
+
+      puts update_cell(@index, @entered_colour)
+      print_board
     end
     mastermind_winning_condition
   end
